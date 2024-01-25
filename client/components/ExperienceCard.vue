d4f044d4bdcf7d08948f2251f1fad265ec85d918
Luiz Eduardo Gotardo <doginuwu@hotmail.com>
2024-01-25 20:43:40 -0300

    chore: change another padding

--- client/components/ExperienceCard.vue
+++ client/components/ExperienceCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 default-card max-w-80 flex flex-col gap-4 items-center text-center">
+  <div class="px-4 pt-6 pb-8 default-card max-w-80 flex flex-col gap-4 items-center text-center">
     <div class="p-3 aspect-square flex items-center justify-center bg-white/10 rounded-full w-fit h-fit">
       <i class="uil uil-times text-2xl" />
     </div>
