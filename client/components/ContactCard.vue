59655aa8a447859c762ac305a407be94b6f81c1c
Luiz Eduardo Gotardo <doginuwu@hotmail.com>
2024-01-26 23:14:35 -0300

    chore: fix border bug

--- client/components/ContactCard.vue
+++ client/components/ContactCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-start gap-4 py-3 px-5 rounded-xl border border-transparent" :class="{ 'bg-purple-600/10 border-purple-600': active }">
+  <div class="flex items-center justify-start gap-4 py-3 px-5 rounded-xl border" :class="{ 'bg-purple-600/10 border-purple-600': active, 'border-transparent': !active }">
     <i class="uil text-purple-600" :class="icon" />
     <slot />
   </div>
