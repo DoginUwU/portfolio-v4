39a2384c1119e0d312bbf107baeb1f2ab5eccd8c
Luiz E <doginuwu@gmail.com>
2024-06-23 08:06:04 -0300

    chore: add missing cache

--- src/server/api/services/github/projects/[name].ts
+++ src/server/api/services/github/projects/[name].ts
@@ -1,8 +1,17 @@
 import { API_GITHUB_USER } from '~/server/constants/github'
 import type { GithubRepository } from '~/types/github'
+import { cache } from '~/server/utils/cache'
+
+const CACHE_TTL = 60 * 60 * 24 // 24 hours
 
 export default defineEventHandler(async (event): Promise<GithubRepository> => {
   const name = getRouterParam(event, 'name')
+
+  const cachedData = cache.get<GithubRepository>(`github-project-${name}`)
+
+  if (cachedData != null) {
+    return cachedData
+  }
 
   const API_URL = `https://api.github.com/repos/${API_GITHUB_USER}/${name}`
 
@@ -13,5 +22,9 @@ export default defineEventHandler(async (event): Promise<GithubRepository> => {
   const responseReadme = await fetch(README_URL)
   const dataReadme = await responseReadme.json()
 
-  return formatGithubRepository(data, dataReadme)
+  const formatedData = formatGithubRepository(data, dataReadme)
+
+  cache.set(`github-project-${name}`, formatedData, CACHE_TTL)
+
+  return formatedData
 })
