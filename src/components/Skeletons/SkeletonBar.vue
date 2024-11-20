e4554ce46425e258dbe8dc99bb66b9951bcc4591
Luiz E. <doginuwu@gmail.com>
2024-11-20 16:32:43 -0300

    feat: add initial astro structure

--- src/components/Skeletons/SkeletonBar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="flex w-full h-2 bg-gray-600 rounded-xl animate-pulse" />
-</template>
--- src/components/Tag.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-purple-200 bg-purple-600/40 rounded-xl text-xs py-1 px-3">
-    <slot />
-  </div>
-</template>
