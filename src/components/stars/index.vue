73668179c83fe954e771fe9e1db95f55a353f0f6
Luiz E. <doginuwu@gmail.com>
2024-11-21 23:14:04 -0300

    feat: add experience cards

---
+++ src/components/stars/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <canvas ref="canvas" class="absolute top-0 w-screen h-full -z-10" />
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { Stars } from './helper'
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+
+onMounted(async () => {
+    await nextTick()
+
+    if (canvas.value === null) { return }
+
+    const helper = new Stars(canvas.value)
+    helper.init()
+})
+</script>
